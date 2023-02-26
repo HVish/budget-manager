02283a2e28ce67b06cab4c6d12725f706de6baf9
Vishnu Singh <vishnusingh1729@gmail.com>
2023-02-26 22:14:03 +0530

    redirect to login page on 401 error

--- client/src/shared/request.ts
+++ client/src/shared/request.ts
@@ -1,7 +1,19 @@
 import axios from 'axios';
+import { clearSession } from './session';
 
 const request = axios.create({
   baseURL: '/api',
 });
+
+request.interceptors.response.use(
+  response => response,
+  err => {
+    if (axios.isAxiosError(err) && err.response?.status === 401) {
+      clearSession();
+      window.location.replace('/auth');
+    }
+    return Promise.reject(err);
+  }
+);
 
 export default request;
